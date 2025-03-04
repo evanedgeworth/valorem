3b564de4c244b847d57531ba5cec173f4b7723c6
BrianCarnes <devmrcarnes@gmail.com>
2025-03-03 18:05:31 -0600

    feat: property rooms

--- src/app/(dashboard)/properties/components/newProperty.modal.tsx
+++ src/app/(dashboard)/properties/components/newProperty.modal.tsx
@@ -8,6 +8,8 @@ import PropertyForm, { PropertyInput } from "./propertyForm";
 import uploadFiles from "@/utils/uploadFile";
 import { useQueryClient } from "@tanstack/react-query";
 import { useToast } from "@/context/toastContext";
+import RoomForm from "./roomForm";
+import { Room, RoomType } from "@/types";
 
 export default function NewPropertyModal({ showModal, setShowModal }: { showModal: boolean; setShowModal: (value: boolean) => void }) {
   const rootRef = useRef<HTMLDivElement>(null);
@@ -15,43 +17,36 @@ export default function NewPropertyModal({ showModal, setShowModal }: { showModa
   const { selectedOrganization } = useContext(UserContext);
   const queryClient = useQueryClient();
   const { showToast } = useToast();
+  const [data, setData] = useState<any>({});
+  const [step, setStep] = useState(1);
 
-  async function handleCreateProperty(data: PropertyInput) {
+  async function handleCreateProperty(data: PropertyInput & { rooms: Room[] }) {
     try {
       setIsLoading(true);
-
-      const numberOfBathroomsTextFormatted = data.noOfBathrooms.replace(" 1/2", "").trim();
-      const numberOfBathrooms = data.noOfBathrooms.toString().includes("1/2")
-        ? Number(numberOfBathroomsTextFormatted) + 1
-        : Number(numberOfBathroomsTextFormatted);
 
       const frontImages = data.frontImages ? await uploadFiles(data.frontImages.map((item) => item.data)) : [];
       const backImages = data.backImages ? await uploadFiles(data.backImages.map((item) => item.data)) : [];
       const leftImages = data.leftImages ? await uploadFiles(data.leftImages.map((item) => item.data)) : [];
       const rightImages = data.rightImages ? await uploadFiles(data.rightImages.map((item) => item.data)) : [];
 
+      const roomsWithImages = await Promise.all(
+        data.rooms.map(async (room) => ({
+          name: room.name,
+          type: room.type,
+          roomImages: await uploadFiles(room.roomImages.map(item => item.data)).then((images) => images.map((image) => ({ fileId: image.key }))),
+        }))
+      );
+
       const res = await request({
         url: `/properties`,
         method: "POST",
         data: {
-          name: data.name,
-          organizationId: selectedOrganization?.organizationId,
-          accessInstructions: data.accessInstructions,
-          notes: data.notes,
-          type: data.type,
-          noOfRooms: Number(data.noOfRooms),
-          noOfBathrooms: numberOfBathrooms,
-          address: {
-            address1: data.address1,
-            address2: data.address2,
-            city: data.city,
-            state: data.state,
-            postalCode: data.postalCode,
-          },
+          ...data,
           frontImages: frontImages.map((item) => ({ fileId: item.key })),
           backImages: backImages.map((item) => ({ fileId: item.key })),
           leftImages: leftImages.map((item) => ({ fileId: item.key })),
           rightImages: rightImages.map((item) => ({ fileId: item.key })),
+          rooms: roomsWithImages,
         },
       });
       if (res?.status === 200) {
@@ -68,6 +63,48 @@ export default function NewPropertyModal({ showModal, setShowModal }: { showModa
     }
   }
 
+  const handleNextStep = (data: PropertyInput) => {
+    const numberOfBathroomsTextFormatted = data.noOfBathrooms.replace(" 1/2", "").trim();
+    const numberOfBathrooms = data.noOfBathrooms.toString().includes("1/2")
+      ? Number(numberOfBathroomsTextFormatted) + 1
+      : Number(numberOfBathroomsTextFormatted);
+
+
+      const bedroomImages = Object.fromEntries(
+        Array.from({ length: Number(data.noOfRooms) }, (_, index) => [`Bedroom ${index + 1}`, []])
+      );
+      const bathroomImages = Object.fromEntries(
+        Array.from({ length: numberOfBathrooms }, (_, index) => {
+          if (data.noOfBathrooms.toString().includes("1/2") && index === numberOfBathrooms - 1) {
+            return [`Half Bath 1`, []];
+          }
+          return [`Bathroom ${index + 1}`, []];
+        })
+      );
+    setData({
+      name: data.name,
+      organizationId: selectedOrganization?.organizationId,
+      accessInstructions: data.accessInstructions,
+      notes: data.notes,
+      type: data.type,
+      noOfRooms: Number(data.noOfRooms),
+      noOfBathrooms: numberOfBathrooms,
+      address: {
+        address1: data.address1,
+        address2: data.address2,
+        city: data.city,
+        state: data.state,
+        postalCode: data.postalCode,
+      },
+      rooms: [
+        ...Object.entries(bedroomImages).map(([key, value]) => ({ name: key, type: RoomType.BEDROOM, roomImages: value })),
+        ...Object.entries(bathroomImages).map(([key, value]) => ({ name: key, type: RoomType.BATHROOM, roomImages: value })),
+      ],
+    });
+
+    setStep(2);
+  }
+
   return (
     <div ref={rootRef}>
       <Button color={"gray"} onClick={() => setShowModal(true)}>
@@ -78,7 +115,30 @@ export default function NewPropertyModal({ showModal, setShowModal }: { showModa
           <h3 className="text-xl font-medium">New Property</h3>
         </Modal.Header>
         <Modal.Body>
-          <PropertyForm onSubmit={handleCreateProperty} isLoading={isLoading} onClose={() => setShowModal(false)} />
+          {
+            step === 1 && (
+              <PropertyForm
+                onSubmit={handleNextStep}
+                isLoading={isLoading}
+                onClose={() => setShowModal(false)}
+              />
+            )
+          }
+          {
+            step === 2 && (
+              <RoomForm
+                rooms={data.rooms || []}
+                isLoading={isLoading}
+                onClose={() => setShowModal(false)}
+                onSubmit={({ rooms }) => {
+                  handleCreateProperty({
+                    ...data,
+                    rooms
+                  });
+                }}
+              />
+            )
+          }
         </Modal.Body>
       </Modal>
     </div>
---
+++ src/app/(dashboard)/properties/components/roomForm.tsx
@@ -0,0 +1,113 @@
+import { Room, RoomType } from "@/types";
+import { Button, Card, Label, Select, TextInput } from "flowbite-react";
+import { Controller, useFieldArray, useForm } from "react-hook-form";
+import ImageInput from "./image-input";
+import { DeleteIcon } from "@/components/icon";
+
+type RoomFormInput = {
+  rooms: Room[];
+}
+
+type RoomFormProps = {
+  rooms: Room[];
+  onSubmit: (value: RoomFormInput) => void;
+  isLoading: boolean;
+  onClose: () => void;
+}
+
+export default function RoomForm({
+  rooms,
+  onSubmit,
+  isLoading,
+  onClose,
+}: RoomFormProps) {
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    control,
+    watch,
+    formState: { errors },
+  } = useForm<RoomFormInput>({ defaultValues: { rooms } });
+
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'rooms',
+  });
+
+  console.log(fields);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="flex justify-between items-center mb-2">
+        <p className="text-2xl">Areas</p>
+        <Button
+          color="gray"
+          onClick={() => append({ name: `Room ${fields.length}`, type: RoomType.BEDROOM, roomImages: [] })}
+        >
+          Add Area
+        </Button>
+      </div>
+      <div>
+        {
+          fields.map((room, index) => (
+            <Card key={room.name + index} className="mb-2 relative">
+              <button
+                className="absolute top-3 right-3"
+                onClick={() => remove(index)}
+              >
+                <DeleteIcon />
+              </button>
+              <div className="space-y-2">
+                <div>
+                  <Label>Name</Label>
+                  <Controller
+                    name={`rooms.${index}.name`}
+                    control={control}
+                    render={({ field }) => <TextInput {...field} placeholder="Name" />}
+                  />
+                </div>
+                <div>
+                  <Label>Type</Label>
+                  <Controller
+                    name={`rooms.${index}.type`}
+                    control={control}
+                    render={({ field }) => (
+                      <Select {...field}>
+                        {Object.values(RoomType).map((item) => (
+                          <option value={item} key={item}>
+                            {item}
+                          </option>
+                        ))}
+                      </Select>
+                    )}
+                  />
+                </div>
+                <div>
+                  <Label>Images</Label>
+                  <Controller
+                    name={`rooms.${index}.roomImages`}
+                    control={control}
+                    render={({ field }) => (
+                      <ImageInput value={field.value} onChange={(value) => setValue(`rooms.${index}.roomImages`, value)} />
+                    )}
+                  />
+                </div>
+              </div>
+            </Card>
+          ))
+        }
+      </div>
+      <div className="flex gap-4 mt-4">
+        <Button type="button" onClick={onClose} fullSized outline color="gray">
+          Close
+        </Button>
+        <Button disabled={isLoading} isProcessing={isLoading} type="submit" fullSized color="gray">
+          {"Save"}
+        </Button>
+      </div>
+    </form>
+  );
+}
